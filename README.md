dd2b7c934ee915e4c323b97b535d1695443891fb
sthanleyso <sthanleys7@gmail.com>
2019-06-12 14:50:02 -0300

    Adicionado o núcleo base da API para realização dos primeiros testes e integração com o front-end

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rocketgram Backend
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3333;
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb://localhost:27017/rocketgram', {
+  useNewUrlParser: true
+});
+
+app.use((req, res) => {
+  req.io = io;
+
+  next();
+});
+
+app.use(cors());
+
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))
+);
+
+app.use(require('./routes'));
+
+server.listen(PORT);
